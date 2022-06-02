8f6f65073c94d3d14638a8610b848da1c3c8db48
Luis Espana <fdh.luis.angel@gmail.com>
2022-06-02 01:29:59 -0500

    :lipstick: Cosmetic

--- src/App.js
+++ src/App.js
@@ -10,20 +10,39 @@ import { handleDownloadImage } from './imageCreator';
 function App() {
   const printableRef = React.useRef();
   const [text, setText] = useState('');
+  const [hashtag, setHashtag] = useState('');
   const onTextChange = (event) => {
     setText(event.target.value.toLowerCase());
+  };
+  const onHashtagChange = (event) => {
+    setHashtag(event.target.value.replace(' ', ''));
   };
 
   return (
     <div className='App'>
+      <nav class="navbar navbar-dark bg-dark sticky-top bg-light">
+        <div className="container-fluid">
+          <div className="navbar-brand">Nombre mamerto generator</div>
+        </div>
+      </nav>
       <Header />
       <div className='inputs-container'>
-        <input
-          type='text'
-          className='name-field'
-          placeholder='Ingresa tu nombre'
-          onChange={onTextChange}
-        />
+        <div class="mb-3">
+          <input
+            type="text"
+            class="form-control mb-3"
+            id="name"
+            placeholder="Ingresa tu nombre"
+            onChange={onTextChange}
+          />
+          <input
+            type="text"
+            class="form-control"
+            id="hastag"
+            placeholder="#"
+            onChange={onHashtagChange}
+          />
+        </div>
       </div>
       <div ref={printableRef} className='container'>
         <div className='letter-box'>
@@ -39,12 +58,21 @@ function App() {
             <Logo className={'loguito'} />
           )}
         </div>
+        <div className='hashtag'>{hashtag}</div>
         <img src={logoPacto} alt='' className='logoPacto' />
-        <DownloadIcon 
+      </div>
+      <DownloadIcon 
           className='download-icon'
           onClick={() => handleDownloadImage(printableRef)}
-        />
-      </div>
+       />
+       <div className='row footer p-0'>
+            <div className='col-2 p-4'>Colaboradores</div>
+            <div className='col p-3'>
+              <div className='row'>Luis </div>
+              <div className='row'>Juan</div>
+              <div className='row'>Daniel </div>
+            </div>
+       </div>
     </div>
   );
 }
