8ca6adb9792fae2a9f1877a879662c67ecb8d7b3
juanpabloxk <xleed@protonmail.ch>
2022-06-01 22:43:07 -0500

    Download image feature implemented

--- src/App.js
+++ src/App.js
@@ -1,48 +1,52 @@
 import './App.scss';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Letter from './Letter';
+import Header from './Header';
 import Logo from './Logo';
-import logoPacto from './logo.png'
+import logoPacto from './logo.png';
+import { handleDownloadImage } from './imageCreator';
+
 function App() {
-
+  const printableRef = React.useRef();
   const [text, setText] = useState('');
-
-  const onChange = (event) => {
+  const onTextChange = (event) => {
     setText(event.target.value.toLowerCase());
   };
 
   return (
     <div className="App">
-      <div className='header'>
-        <a 
-          href="https://www.linkedin.com/in/luis-angel-espa%C3%B1a-yepez-a384471a5/"
-          target={'_blank'} rel="noreferrer">
-          <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" alt="" className='linkedin'/>
-        </a>       
+      <Header />
+      <div className="inputs-container">
+        <input
+          type="text"
+          className="nameField"
+          placeholder="Ingresa tu nombre"
+          onChange={onTextChange}
+        />
+        <button
+          className="download-button"
+          type="button"
+          onClick={() => handleDownloadImage(printableRef)}
+        >
+          Descargar imagen
+        </button>
       </div>
-      <input 
-        type="text"
-        className='nameField'
-        placeholder='Ingresa tu nombre'
-        onChange={onChange}
-      />
-
-      <div className='letter-box'>
-        {
-          text.split('').map((l, i) => 
+      <div ref={printableRef} className="container">
+        <div className="letter-box">
+          {text.split('').map((l, i) => (
             <Letter
               key={i}
-              letter={l === 'o' && i < text.length -1 ? 'O' : l}
-              index={i}/>
-          )
-        }
+              letter={l === 'o' && i < text.length - 1 ? 'O' : l}
+              index={i}
+            />
+          ))}
 
-        {
-          text.charAt(text.length-1) !== 'o' && 
-          <Logo className={'loguito'}/>
-        }
+          {text.charAt(text.length - 1) !== 'o' && (
+            <Logo className={'loguito'} />
+          )}
+        </div>
+        <img src={logoPacto} alt="" className="logoPacto" />
       </div>
-      <img src={logoPacto} alt="" className='logoPacto'/>
     </div>
   );
 }
